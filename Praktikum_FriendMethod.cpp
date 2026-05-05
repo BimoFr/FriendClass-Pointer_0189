81286a7794b49e097812535afc86a445958d5a17
BimoFr <bimofadhlurrahman@gmail.com>
2026-05-05 10:31:21 +0700

    Menambahkan implementasi fungsi input, luas, keliling, dan output pada class LayangLayang

--- Praktikum_FriendMethod.cpp
+++ Praktikum_FriendMethod.cpp
@@ -37,4 +37,28 @@ class LayangLayang {
 private:
     float d1, d2, s1, s2;
 
+public:
+    void input() {
+        cout << "--- Input Layang-Layang ---" << endl;
+        cout << "Diagonal 1: "; cin >> d1;
+        cout << "Diagonal 2: "; cin >> d2;
+        cout << "Sisi 1: "; cin >> s1;
+        cout << "Sisi 2: "; cin >> s2;
+    }
+
+    float luas() {
+        return 0.5 * d1 * d2;
+    }
+
+    float keliling() {
+        return 2 * (s1 + s2);
+    }
+
+    void output() {
+        cout << "--- Output Layang-Layang ---" << endl;
+        cout << "Luas: " << luas() << endl;
+        cout << "Keliling: " << keliling() << endl;
+    }
+
+    friend float BelahKetupat::kelilingLayang(LayangLayang &l);
 };
