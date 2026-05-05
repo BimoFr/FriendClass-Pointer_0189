f2448c4fd7aa7450eb6089c896d04d303ca7aefb
BimoFr <bimofadhlurrahman@gmail.com>
2026-05-05 10:30:13 +0700

    Menambahkan implementasi fungsi input, luas, keliling, dan output pada class BelahKetupat di file Praktikum_FriendMethod.cpp

--- Praktikum_FriendMethod.cpp
+++ Praktikum_FriendMethod.cpp
@@ -8,4 +8,27 @@ class BelahKetupat {
 private:
     float d1, d2, s;
 
+public:
+    void input() {
+        cout << "--- Input Belah Ketupat ---" << endl;
+        cout << "Diagonal 1: "; cin >> d1;
+        cout << "Diagonal 2: "; cin >> d2;
+        cout << "Sisi: "; cin >> s;
+    }
+
+    float luas() {
+        return 0.5 * d1 * d2;
+    }
+
+    float keliling() {
+        return 4 * s;
+    }
+
+    float kelilingLayang(LayangLayang &l);
+
+    void output() {
+        cout << "--- Output Belah Ketupat ---" << endl;
+        cout << "Luas: " << luas() << endl;
+        cout << "Keliling: " << keliling() << endl;
+    }
 };
